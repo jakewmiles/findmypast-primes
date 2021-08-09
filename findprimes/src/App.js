e11d9f1bc54c965d712f05fa1e471c97822bad53
jakewmiles <jake.w.miles@gmail.com>
2021-08-10 00:18:29 +0100

    feat: implement slider with useState

--- findprimes/src/App.js
+++ findprimes/src/App.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 function App() {
+  const [N, setN] = useState(1);
+  const handler = (e) => {
+    setN(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h1>Multiplication table of the {N > 1 ? `first ${N} prime numbers`: `first prime number`} 
+          <input type="range" min="1" max="100" value={N} onChange={handler} step="1">
+          </input>
+        </h1>
+        <h2>Drag the slider to see larger/smaller tables! Scroll the page as N increases...</h2>
       </header>
     </div>
   );
