ddf36f951ec818aa05c4e04424abd08a6670dabd
jakewmiles <jake.w.miles@gmail.com>
2021-08-10 00:12:15 +0100

    feat: implement generatePrimeString

--- findprimes/src/helperFunctions.js
+++ findprimes/src/helperFunctions.js
@@ -19,6 +19,31 @@ const firstNPrimes = (n) => {
   return primeList;
 }
 
+const generatePrimeString = (primeList) => {
+  let primeString = 'x-';
+  let initial = -1;
+  for (let i = initial; i < primeList.length; i++) {
+    for (let j = initial; j < primeList.length; j++) {
+      if (i === initial && j > initial) {
+        primeString += delimitThousands(primeList[j]) + '-';
+      } 
+      else if (j === initial && i >- 1) {
+        primeString += delimitThousands(primeList[i]) + '-';
+      } 
+      else if (i > initial && j >- 1) {
+        primeString += delimitThousands(primeList[i]*primeList[j]) + '-';
+      }
+    }
+    primeString += '\n';
+  }
+  return primeString;
+};
+
+const delimitThousands = (n) => {
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 exports.primeTester = primeTester;
 exports.firstNPrimes = firstNPrimes;
-
+exports.generatePrimeString = generatePrimeString;
+exports.delimitThousands = delimitThousands;
--- findprimes/src/test/test.js
+++ findprimes/src/test/test.js
@@ -3,6 +3,7 @@ const mocks = require('./mocks').mocks;
 
 const primeTester = functions.primeTester;
 const firstNPrimes = functions.firstNPrimes;
+const delimitThousands = functions.delimitThousands;
 
 require('chai').should();
 
@@ -16,5 +17,16 @@ describe('primeTester', () => {
 describe('firstNPrimes', () => {
   it('should return an array of primes up to the Nth prime', function () {
     firstNPrimes(10).should.eql(mocks.nPrimes);
+  });
+});
+
+describe('delimitThousands', () => {
+  it('should return numbers above 3 digits delimited with commas', function () {
+    delimitThousands(10).should.eql('10');
+    delimitThousands(100).should.eql('100');
+    delimitThousands(1000).should.eql('1,000');
+    delimitThousands(10000).should.eql('10,000');
+    delimitThousands(100000).should.eql('100,000');
+    delimitThousands(1000000).should.eql('1,000,000');
   });
 });
